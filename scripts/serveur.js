a1994486f2ce56cac489a7ba567a6da8df59e940
YoLaub <johngreta904@gmail.com>
2025-01-20 23:26:45 +0100

    récuperation de données météo

--- scripts/serveur.js
+++ scripts/serveur.js
@@ -1,2 +1,26 @@
-const URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&forecast_days=14"
+// const GEOCODING = "https://geocoding-api.open-meteo.com/v1/search?name={Berlin}&count=10&language=en&format=json";
+
+var form = document.querySelector("#weatherForm");
+var context = document.querySelector("#weatherChart");
+
+var weather_chart;
+
+form.addEventListener('submit', async (e)=>{
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const latitude = formData.get("latitude");
+    const longitude = formData.get("longitude");
+    const hourly = formData.get("hourly");
+    const forecastDays = formData.get("forecast_days");
+
+
+    const WEATHER = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=${hourly}&forecast_days=${forecastDays}`;;
+
+    const response = await fetch(WEATHER);
+    const data = await response.json();
+
+    console.log(data)
+
+})
 
